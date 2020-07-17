fcbe685ca75cc357661a222a2a3515bff0e0bb90
Josh Hill <josh.hill189@gmail.com>
2020-07-18 11:37:30 +1200

    Add Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import Book from "./pages/Book";
 import Class from "./pages/Class";
@@ -11,11 +11,66 @@ import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import Rewards from "./pages/Rewards";
 
+import { getUser } from "./api/user";
+
 import "./App.css";
 
 const App = () => {
+  console.log(getUser().then((resp) => resp.body));
   return (
     <Router>
+      <div className="custom-wrapper pure-g" id="menu">
+        <div className="pure-u-1 pure-u-md-6-24">
+          <div className="pure-menu">
+            <Link to="/" className="pure-menu-heading custom-brand">
+              Upskill
+            </Link>
+            <a
+              href="#"
+              aria-label="None"
+              className="custom-toggle"
+              id="toggle"
+              style={{ height: "2.5em" }}
+            >
+              <s className="bar"></s>
+              <s className="bar"></s>
+            </a>
+          </div>
+        </div>
+        <div className="pure-u-1 pure-u-md-12-24">
+          <div className="pure-menu pure-menu-horizontal custom-can-transform centered">
+            <ul className="pure-menu-list">
+              <li className="pure-menu-item">
+                <Link to="/instructors" className="pure-menu-link">
+                  Instructors
+                </Link>
+              </li>
+              <li className="pure-menu-item">
+                <Link to="/classes" className="pure-menu-link">
+                  Classes
+                </Link>
+              </li>
+              <li className="pure-menu-item">
+                <Link to="/rewards" className="pure-menu-link">
+                  Rewards
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="pure-u-1 pure-u-md-6-24">
+          <div className="pure-menu pure-menu-horizontal custom-menu-3">
+            <ul className="pure-menu-list">
+              <li className="pure-menu-item">
+                <Link to="/login" className="pure-menu-link">
+                  Login
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
       <Switch>
         <Route exact path="/" component={Landing} />
         {/* The login page */}
