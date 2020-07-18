45f3755776acf7e111f7775223f11b251f0b8c83
KimberleyEvans-Parker <kim@evans-parker.com>
2020-07-18 15:25:23 +1200

    Add Cards

--- frontend/src/pages/Instructor.tsx
+++ frontend/src/pages/Instructor.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import JohnSmith from "../assets/John Smith.jpg";
 import "./Instructor.css";
+import { ClassCard } from "../components/Card";
 
 import StarRatingComponent from "react-star-rating-component";
+
+import testImage from "../assets/test.webp";
 
 interface OwnProps {
   instructorId: number;
@@ -26,7 +29,7 @@ const Instructor = (props: InstructorProps) => {
       />
       {/* <img src={require(props.image)} alt={props.name} /> */}
       {/* <img src={JohnSmith} alt={props.name} /> */}
-      <h1>{props.name}</h1>
+      <h1 className="instructor-heading">{props.name}</h1>
       <StarRatingComponent
         name={"rating"}
         value={props.stars}
@@ -37,7 +40,44 @@ const Instructor = (props: InstructorProps) => {
       {props.description.split("\\n").map((line: string) => {
         return <p>{line}</p>;
       })}
-      {/* <p className="description">{props.description}</p> */}
+      <h2>Available Classes</h2>
+      <ClassCard
+        name="Physics"
+        image={testImage}
+        cost={20}
+        description="Come and learn Physics!"
+      />
+      <ClassCard
+        name="Blockchain"
+        image={testImage}
+        cost={30}
+        description="Come and learn blockchain!"
+      />
+      <ClassCard
+        name="Start up"
+        image={testImage}
+        cost={25}
+        description="Come and learn how to make a great start up!"
+      />
+      <h2>Classes I've taken</h2>
+      <ClassCard
+        name="Linguistics"
+        image={testImage}
+        cost={40}
+        description="Learn a new language!"
+      />
+      <ClassCard
+        name="4 Dimensions"
+        image={testImage}
+        cost={30}
+        description="Come and learn how bigger on the inside is possible with a fourth dimension!"
+      />
+      <ClassCard
+        name="Parallel Universe"
+        image={testImage}
+        cost={25}
+        description="Do they exist? Discuss theories on how travelling accross parallel universes is possible."
+      />
       <p>You're viewing the page for instructor {props.instructorId}</p>
     </div>
   );
