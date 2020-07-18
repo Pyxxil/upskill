d0075ae21f45f04f295e357380fce8357ca671e6
Josh Hill <josh.hill189@gmail.com>
2020-07-19 00:41:08 +1200

    Almost Complete Styling

--- frontend/src/pages/CompanyChallenge.tsx
+++ frontend/src/pages/CompanyChallenge.tsx
@@ -1,25 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 interface OwnProps {
-    challengeId: String;
-    name: String;
-    company: String;
-    description: String;
+  challengeId: String;
+  name: String;
+  company: String;
+  description: String;
 }
 
 type CompanyChallengeProps = OwnProps;
 
 const CompanyChallenge = (props: CompanyChallengeProps) => {
-    return (
-        <div className="content">
-            <h1>{props.name}</h1>
+  return (
+    <div className="content">
+      <h1>{props.name}</h1>
 
-            <p>Posted by {props.company}</p>
+      <p>Posted by {props.company}</p>
 
-            <p>{props.description}</p>
-        </div>
-    );
+      <p>{props.description}</p>
+    </div>
+  );
 };
 
 export default CompanyChallenge;
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,20 +1,19 @@
 import React, { FormEvent } from "react";
-import { User } from "../App";
-import { debug } from "console";
-import { loginUser } from "../utils/userRequests";
-import { AxiosError, AxiosResponse } from 'axios';
 import { RouteComponentProps, withRouter, Redirect } from "react-router-dom";
 
+import { User } from "../App";
+import { loginUser } from "../utils/userRequests";
+
 interface IProps extends RouteComponentProps<any> {
-  user: User | null | undefined,
-  setAppState: Function
+  user: User | null | undefined;
+  setAppState: Function;
 }
 
 interface IState {
-  email?: string,
-  password?: string,
+  email?: string;
+  password?: string;
 
-  error?: string[]
+  error?: string[];
 }
 
 class Login extends React.Component<IProps, IState> {
@@ -25,8 +24,8 @@ class Login extends React.Component<IProps, IState> {
       email: "",
       password: "",
 
-      error: []
-    }
+      error: [],
+    };
   }
 
   handleChangeEmail(event: FormEvent<HTMLInputElement>) {
@@ -40,19 +39,21 @@ class Login extends React.Component<IProps, IState> {
   /**
    * Submit logic.
    * When the user presses the login button, a request is made to the backend to attempt to login.
-   * @param event 
+   * @param event
    */
   handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     if (this.validateClientSide()) {
-      console.log("Submitting login form");
-      console.log(this.state);
-      loginUser(this.state.email, this.state.password).then((res) => {
-        this.props.setAppState({ user: res.data.user });
-        this.props.history.push("/profile");
-      }).catch((err) => {
-        this.setState({ error: [err.name + ", " + err.message] })
-      });
+      // console.log("Submitting login form");
+      // console.log(this.state);
+      loginUser(this.state.email, this.state.password)
+        .then((res) => {
+          this.props.setAppState({ user: res.data.user });
+          this.props.history.push("/profile");
+        })
+        .catch((err) => {
+          this.setState({ error: [err.name + ", " + err.message] });
+        });
     }
   }
 
@@ -71,7 +72,7 @@ class Login extends React.Component<IProps, IState> {
     }
 
     this.setState({
-      error: overallError
+      error: overallError,
     });
 
     return valid;
@@ -80,29 +81,49 @@ class Login extends React.Component<IProps, IState> {
   render() {
     // Redirect if there exists logged in user
     if (this.props.user) {
-      return <Redirect to="/" />
+      return <Redirect to="/" />;
     }
     return (
-      <div>
-        <h1>Please register</h1>
+      <div className="content centered">
+        <h1>Please login</h1>
         <div>
-          {this.state.error && this.state.error.length > 0 && (<h2>Errors:</h2>)}
-          {this.state.error && this.state.error.map((errorReason, index) => (
-            <li key={index}>
-              {errorReason}
-            </li>
-          ))}
+          {this.state.error && this.state.error.length > 0 && <h2>Errors:</h2>}
+          {this.state.error &&
+            this.state.error.map((errorReason, index) => (
+              <li key={index}>{errorReason}</li>
+            ))}
         </div>
-        <form onSubmit={this.handleSubmit.bind(this)}>
+        <form
+          onSubmit={this.handleSubmit.bind(this)}
+          className="pure-form pure-form-stacked"
+        >
           <label htmlFor="email">Email</label>
-          <input type="text" id="email" name="email" onChange={this.handleChangeEmail.bind(this)} value={this.state.email} /><br />
+          <input
+            type="text"
+            id="email"
+            name="email"
+            onChange={this.handleChangeEmail.bind(this)}
+            value={this.state.email}
+            style={{ display: "inline-block" }}
+          />
+          <br />
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" name="password" onChange={this.handleChangePassword.bind(this)} value={this.state.password} /><br />
-          <input type="submit" value="Submit" />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            onChange={this.handleChangePassword.bind(this)}
+            value={this.state.password}
+            style={{ display: "inline-block" }}
+          />
+          <br />
+          <button type="submit" value="Submit" className="pure-button">
+            Login
+          </button>
         </form>
       </div>
     );
   }
-};
+}
 
 export default withRouter(Login);
