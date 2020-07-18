b1201bc038d84c58ccce6f382157da3b707688dc
Terence Qu <Terenco12345@gmail.com>
2020-07-18 18:33:47 +1200

    Added new cards
    - New cards for Centrality and Potentia

--- frontend/src/pages/CompanyChallenges.tsx
+++ frontend/src/pages/CompanyChallenges.tsx
@@ -3,19 +3,33 @@ import React from "react";
 import { ChallengeCard } from "../components/Card";
 
 import devsImage from "../assets/devs.png";
+import potentiaImage from "../assets/potentia.png";
+import centralityImage from "../assets/centrality.jpg";
 
 const CompanyChallenges = () => {
-  return (
-    <div>
-      <h1>Here's a list of our challenges.</h1>
-      <ChallengeCard
-        name="Super Hard Challenge"
-        image={devsImage}
-        instructor="DEVS"
-        description="This is a super hard challenge."
-      ></ChallengeCard>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Here's a list of our challenges.</h1>
+            <ChallengeCard
+                name="48 Hour Challenge"
+                image={devsImage}
+                instructor="DEVS"
+                description="This is a super hard challenge."
+            ></ChallengeCard>
+            <ChallengeCard
+                name="Potentia Challenge"
+                image={potentiaImage}
+                instructor="Potentia"
+                description="This is an extra hard challenge."
+            ></ChallengeCard>
+            <ChallengeCard
+                name="Centrality Challenge"
+                image={centralityImage}
+                instructor="Centrality"
+                description="This is an extra hard challenge."
+            ></ChallengeCard>
+        </div>
+    );
 };
 
 export default CompanyChallenges;
