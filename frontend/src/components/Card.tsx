f2919377e4a0a7013b47bc870504673116ada90b
KimberleyEvans-Parker <kim@evans-parker.com>
2020-07-19 13:07:26 +1200

    Add times

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -49,6 +49,7 @@ export const InstructorCard = (props: InstructorCardProps) => {
 interface ClassCardProps {
   name: String;
   instructor: String;
+  time: String;
   image: string;
   cost: number;
   description: String;
@@ -63,6 +64,7 @@ export const ClassCard = (props: ClassCardProps) => {
       <h1 className="card-title">{props.name}</h1>
       <div className="card-content">
         <p>{props.instructor}</p>
+        <p>{props.time}</p>
         <p>${props.cost}</p>
         <p>{props.description}</p>
       </div>
--- frontend/src/pages/Classes.tsx
+++ frontend/src/pages/Classes.tsx
@@ -32,6 +32,7 @@ const Classes = () => {
         name="Blockchain"
         instructor="Ryan Tan"
         image={blockchain}
+        time="1pm-2pm"
         cost={40}
         description="This specialization introduces blockchain, a revolutionary technology that enables peer-to-peer transfer of digital assets without any intermediaries, and is predicted to be just as impactful as the Internet. "
       />
@@ -39,6 +40,7 @@ const Classes = () => {
         name="Binary"
         instructor="Josh Hill"
         image={binary}
+        time="3pm-4pm"
         cost={25}
         description="An introduction to binary and how it is used in a computer.
           You'll learn how there are 10 kinds of people in this world; those who understand binary, those who don't, and those who fall prey to off by one errors."
@@ -47,6 +49,7 @@ const Classes = () => {
         name="Artificial Intelligence"
         instructor="Sunny Feng"
         image={AI}
+        time="1pm-2pm"
         cost={30}
         description="Learn the powers and dangers of AI and where it can take you.
         Additionally, this classcovers opportunities for AI, as well as ethical concerns surounding AI.  "
@@ -55,6 +58,7 @@ const Classes = () => {
         name="Networking"
         instructor="Ryan Tan"
         image={networking}
+        time="2pm-3pm"
         cost={35}
         description="Learn how computers network, while networking with networkers.
           During this course, you will learn the fundamentals of modern networking technologies and protocols.  "
@@ -63,6 +67,7 @@ const Classes = () => {
         name="Money making"
         instructor="Kimberley Evans-Parker"
         image={money}
+        time="11am-12pm"
         cost={25}
         description="Do you want your coding to convert into currency?
           Learn how programming can earn you money.
@@ -72,6 +77,7 @@ const Classes = () => {
         name="Machine Learning"
         instructor="Hassaan Mirza"
         image={purpleComputer}
+        time="10am-11am"
         cost={27.5}
         description="Machine learning has given us self-driving cars, practical speech recognition, effective web search, and a vastly improved understanding of the human genome.
           Many think it is the best way to make progress towards human-level AI."
@@ -80,6 +86,7 @@ const Classes = () => {
         name="Neural Networks"
         instructor="Ryan Tan"
         image={neuralNetworks}
+        time="1pm-2pm"
         cost={30}
         description="Deep learning engineers are highly sought after, and mastering deep learning will give you numerous new career opportunities.
           This course will teach you how to to build, train and apply deep neural networks."
@@ -88,6 +95,7 @@ const Classes = () => {
         name="FPGAs"
         instructor="Hassaan Mirza"
         image={ICT}
+        time="3pm-4pm"
         cost={25}
         description="Do you want to learn how to describe hardware?
           Want to be able to perform cool memory and interupt based projects on a programable logic device?
@@ -97,6 +105,7 @@ const Classes = () => {
         name="Start Up"
         instructor="Terence Qu"
         image={idea}
+        time="4pm-5pm"
         cost={25}
         description="Learn how to create your own start up!
           In a world where jobs are scarce, learn how to create your own!"
